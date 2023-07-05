98da01a6fc1451bec817001468eac365ec6f9f75
Attash <attash.ahmedi@gmail.com>
2023-07-05 18:17:52 -0500

    Added erase button and color changer

--- source.js
+++ source.js
@@ -1,15 +1,33 @@
 const gridObj = document.querySelector('.grid');
+const colorPicker = document.querySelector('.colorPicker');
+const eraserButton = document.querySelector('.eraser');
+const sliderObj = document.querySelector('.slider');        //obj for actual slider
+let sliderValueObj = document.querySelector('.sliderValue'); //obj for value of slider displayed on screen
+sliderValueObj.textContent = `${sliderObj.value} x ${sliderObj.value}`; 
 
-const sliderObj = document.querySelector('.slider');
-let sliderValueObj = document.querySelector('.sliderValue');
-sliderValueObj.textContent = sliderObj.value; 
+sliderObj.value = 50;          //creates initial grid of 50x50
+gridCreation();
 
-sliderObj.oninput = function() {   //delete old grid, create new grid when slider is adjusted
+
+sliderObj.oninput = function() {   //create new grid when slider is adjusted
+    gridCreation();
+}
+colorPicker.oninput = function() {
+    const squares = document.querySelectorAll('.gridSquare');
+    squares.forEach(gridSquare => gridSquare.addEventListener('mouseover', function() {
+        this.style.cssText = `background-color: ${colorPicker.value}`;
+    }));
+}
+
+eraserButton.addEventListener('click', gridCreation); //erase just creates a new grid of same size
+
+
+function gridCreation() {
     while(gridObj.firstChild) {
         gridObj.removeChild(gridObj.firstChild);
     }
 
-    sliderValueObj.textContent = sliderObj.value;
+    sliderValueObj.textContent = `${sliderObj.value} x ${sliderObj.value}`;
 
     for (let i = 0; i < sliderObj.value; i++) {
         const newRow = document.createElement('div');
@@ -24,31 +42,8 @@ sliderObj.oninput = function() {   //delete old grid, create new grid when slide
         gridObj.appendChild(newRow);
     }
     const squares = document.querySelectorAll('.gridSquare');
-    squares.forEach(gridSquare => gridSquare.addEventListener('mouseover', hover));
-
+    squares.forEach(gridSquare => gridSquare.addEventListener('mouseover', function() {
+        this.style.cssText = `background-color: ${colorPicker.value}`;
+    }));
 }
 
-function initialSetup() {
-    for (let i = 0; i < 50; i++) {
-        const newRow = document.createElement('div');
-        newRow.classList.add('row');
-        
-        for (let x = 0; x < 50; x++) {
-            const squareObj = document.createElement('div');
-            squareObj.classList.add('gridSquare');   
-            newRow.appendChild(squareObj);
-        }
-        
-        gridObj.appendChild(newRow);
-    }
-
-const squares = document.querySelectorAll('.gridSquare');
-squares.forEach(gridSquare => gridSquare.addEventListener('mouseover', hover));
-}
-
-initialSetup();
-
-squares.forEach(gridSquare => gridSquare.de)
-function hover() {
-    this.style.cssText = 'background-color: lightblue';
-}
